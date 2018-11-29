c806546d2ad971db907b2d7d6782ec3dbdcdbad6
Takuma-Goto <tkmgoto94@gmail.com>
2018-11-29 15:55:51 +0900

    DB接続 複数行クエリ 単行クエリ 更新クエリ POST GET

--- README.md
+++ README.md
@@ -7,18 +7,21 @@
 DB接続
 
 ## 前提
-- local pcにgoをインストール
+- host pcにgoをインストール
 - 動作確認端末のバージョン  
 > go version 1.10.3  
 > go version 1.9
 
-- local pcにgo go-sql-driverをインストール
+- host pcにgo go-sql-driverをインストール
 `go get -u github.com/go-sql-driver/mysql`
 
-- local pcにmysqlをインストールしてあること
+- host pcにmysqlをインストールしてあること
 参考 : `https://qiita.com/hkusu/items/cda3e8461e7a46ecf25d`
 localhostのmysqlにはパスワードを設定していないこと
 パスワード設定している場合は、db接続のクラスを修正してください
+
+- あらかじめhost pcのmysqlにデータベース追加する
+`create database gosample;`
 
 ## 起動方法
 ### for local
@@ -28,3 +31,7 @@ localhostのmysqlにはパスワードを設定していないこと
 ## 検証方法
 webブラウザーで下記をURLに入力  
 `http://localhost:8080/index`
+
+## 注意事項
+- ログイン機能が未実装なので、CSRF対策が施されていないので、この知識のままwebアプリケーションを実装しないようにしてください
+- 次章でログイン機能を実装し、様々な脆弱性の対策をしていきます
--- app/controller/app.go
+++ app/controller/app.go
@@ -24,22 +24,54 @@ func Index(rw http.ResponseWriter, request *http.Request) {
 
 // Home is method to render Name page.
 func Home(rw http.ResponseWriter, request *http.Request){
-
 	tmpl := parseTemplate()
 
 	// db connection
 	dbm := db.ConnDB()
-	user := new(models.User)
+	users := []models.User{}
 
-	row := dbm.QueryRow("select id, name from users where id = ?", 1)
+	rows, err := dbm.Query("select id, name from users")
+	for rows.Next() {
+		user := models.User{}
+		if err = rows.Scan(&user.ID, &user.Name); err != nil {
+			// 変換に失敗した場合、INDEX画面表示
+			toIndex(tmpl, rw, request, "ユーザ データ変換 失敗", err)
+			return
+		}
+		users = append(users, user)
+	}
+
+	// HOME画面表示
+	err = tmpl.ExecuteTemplate(rw, "home.html", struct {
+		Users []models.User
+	}{
+		Users: users,
+	})
+	if err != nil {
+		outputErrorLog("HTML 描画 エラー", err)
+	}
+}
+
+// Detail is method to render Detail page.
+func Detail(rw http.ResponseWriter, request *http.Request){
+	tmpl := parseTemplate()
+
+	// ID取得
+	id := request.URL.Query().Get("id")
+	log.Println("id : ", id)
+
+	// db connection
+	dbm := db.ConnDB()
+	user := new(models.User)
+	row := dbm.QueryRow("select id, name from users where id = ?", id)
 	if err := row.Scan(&user.ID, &user.Name); err != nil {
 		// 変換に失敗した場合、INDEX画面表示
 		toIndex(tmpl, rw, request, "ユーザ データ変換 失敗", err)
 		return
 	}
 
-	// HOME画面表示
-	err := tmpl.ExecuteTemplate(rw, "home.html", struct {
+	// Detail画面表示
+	err := tmpl.ExecuteTemplate(rw, "detail.html", struct {
 		User     *models.User
 	}{
 		User:     user,
@@ -47,6 +79,37 @@ func Home(rw http.ResponseWriter, request *http.Request){
 	if err != nil {
 		outputErrorLog("HTML 描画 エラー", err)
 	}
+}
+
+// ユーザー情報更新
+func Edit(rw http.ResponseWriter, request *http.Request){
+	tmpl := parseTemplate()
+
+	// 変更する名前とIDを取得
+	err := request.ParseForm()
+	if err != nil {
+		// パースに失敗したらエラー
+		toIndex(tmpl, rw, request, "フォーム パース エラー", err)
+		return
+	}
+	name := request.Form.Get("name")
+	log.Println("変更する名前：", name)
+
+	id := request.Form.Get("id")
+	log.Println("変更する対象ID：", id)
+
+	// db connection
+	dbm := db.ConnDB()
+	_, err = dbm.Exec("update users set name = ? where id = ?", name, id)
+	if err != nil {
+		log.Println("ユーザー 更新 失敗")
+	} else {
+		log.Println("ユーザー 更新 成功")
+		log.Println("更新したユーザーID：", id)
+	}
+
+	// HOME画面表示
+	http.Redirect(rw, request, "/home", http.StatusFound)
 }
 
 // parse HTML
